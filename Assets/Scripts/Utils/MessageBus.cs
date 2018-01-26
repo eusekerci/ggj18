279348b67ff5b468bb1f16a766b027b598121d0f
yagizarkayin <arkayinyagiz@gmail.com>
2018-01-26 21:35:24 +0300

    connection establish/lost/hit

---
+++ Assets/Scripts/Utils/MessageBus.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public abstract class GGJEvent { }
+
+public static class MessageBus
+{
+    public static void Publish<T>(T evnt) where T : GGJEvent
+    {
+        UniRx.MessageBroker.Default.Publish(evnt);
+    }
+
+    public static UniRx.IObservable<T> OnEvent<T>() where T : GGJEvent
+    {
+        return UniRx.MessageBroker.Default.Receive<T>();
+    }
+}
